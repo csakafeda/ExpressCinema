5f97d6bcd33f7196daa792ca40c5c441bc3ad2f6
csakafeda <bako.feodora97@gmail.com>
2023-07-06 17:38:58 +0200

    add very basic backend

--- server/server.js
+++ server/server.js
@@ -6,7 +6,8 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    console.log("You are now at the '/' endpoint! ");
+    console.log("You are now at the '/' endpoint!");
+    res.sendStatus(200); // Sends a 200 OK response
 });
 
 app.listen(8080, () => {

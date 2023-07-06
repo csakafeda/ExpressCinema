d033fcd19ebce90e2d0af2193fb07c96c669e3ce
csakafeda <bako.feodora97@gmail.com>
2023-07-06 17:31:49 +0200

    add basic backend without mySQL

--- server/server.js
+++ server/server.js
@@ -5,36 +5,10 @@ const app = express();
 
 app.use(express.json());
 
-//app.use("/api/shows", showRouter);
-
-//TODO from .env
-const connection = mysql.createConnection({
-  host: 'your_host',
-  user: 'your_username',
-  password: 'your_password',
-  database: 'your_database',
+app.get('/', (req, res) => {
+    console.log("You are now at the '/' endpoint! ");
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to database:', err);
-    return;
-  }
-  console.log('Connected to the database');
-});
-
-app.get('/data', (req, res) => {
-  // Perform a SQL query ---- table name
-  connection.query('SELECT * FROM your_table', (err, results) => {
-    if (err) {
-      console.error('Error executing query:', err);
-      res.status(500).json({ error: 'Failed to fetch data' });
-      return;
-    }
-    res.json(results);
-  });
-});
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(8080, () => {
+    console.log('Server is running on port 8080');
 });

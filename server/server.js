258842160f98d4427f63a5dbb6c7027877031b0a
csakafeda <bako.feodora97@gmail.com>
2023-07-07 15:50:46 +0200

    move db connection to db.js

--- server/server.js
+++ server/server.js
@@ -1,24 +1,15 @@
 const express = require('express');
-const mysql = require('mysql2');
-require('dotenv').config();
-
 const app = express();
-const seatRouter = require("./routes/seats.router");
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-})
-connection.connect((err) => {
-    if (err) console.log(err);
-    else console.log("Connected to DB")
-})
+const seatRouter = require("./routes/seatRouter");
+const con = require("./config/db.js")
 
 app.use(express.json());
+app.use(function(req, res, next) {
+    req.con = con
+    next()
+})
 
-app.use("/api/seats", seatRouter(connection));
-
+app.use("/api/seats", seatRouter);
 
 app.listen(8080, () => {
     console.log('Server is running on port 8080.');

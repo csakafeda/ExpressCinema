ad795a4137daf118b60be97da7e53757fac1b948
csakafeda <bako.feodora97@gmail.com>
2023-07-07 11:10:01 +0200

    replace hardcoded db to mysql db in seat router, add seat adding request

--- server/routes/seats.router.js
+++ server/routes/seats.router.js
@@ -1,57 +1,94 @@
 const {Router} = require("express");
 const nodemailer = require('nodemailer');
 
-const seatRouter = new Router();
+const seatRouter = (connection) => {
+    const router = new Router();
 
-const seats = [
-    {id: 1, status: 'available'},
-    {id: 2, status: 'available'},
-    {id: 3, status: 'available'},
-    {id: 4, status: 'available'},
-    {id: 5, status: 'available'}
-];
+    router.get("/", async (req, res) => {
+        connection.query(`SELECT *
+                          FROM seats`, (err, results) => {
+            if (err) {
+                console.error(err);
+                res.status(500).json({error: 'Failed to fetch seats.'});
+                return;
+            }
+            res.json(results);
+        });
+    });
 
-seatRouter.get("/", async (req, res) => {
-    res.json(seats);
-});
+    router.post('/reserve', (req, res) => {
+        const seatId = parseInt(req.body.seatId);
+        const userId = req.body.userId;
 
-seatRouter.post('/reserve', (req, res) => {
-    const seatId = parseInt(req.body.seatId);
-    const userId = req.body.userId;
+        connection.query('SELECT * FROM seats WHERE id = ? AND status = "available"', [seatId], (err, results) => {
+            if (err) {
+                console.error(err);
+                res.status(500).json({error: 'Failed to reserve the seat.'});
+                return;
+            }
 
-    const seat = seats.find(seat => seat.id === seatId);
-    if (!seat || seat.status !== 'available') {
-        res.status(400).json({error: 'The seat cannot be reserved.'});
-        return;
-    }
-    seat.status = "reserved";
-    seat["userId"] = userId;
+            if (results.length === 0) {
+                res.status(400).json({error: 'The seat cannot be reserved.'});
+                return;
+            }
 
-    res.json({message: 'Reservation successful.'});
-});
+            connection.query('UPDATE seats SET status = "reserved", userId = ? WHERE id = ?', [userId, seatId], (err, updateResult) => {
+                if (err) {
+                    console.error(err);
+                    res.status(500).json({error: 'Failed to reserve the seat.'});
+                    return;
+                }
 
-seatRouter.post('/pay', (req, res) => {
-    const seatId = parseInt(req.body.seatId);
-    const userId = parseInt(req.body.userId);
-    const userEmail = req.body.email;
+                res.json({message: 'Reservation successful.'});
+            });
+        });
+    });
 
-    const seat = seats.find(seat => seat.id === seatId);
-    if (!seat || seat.status !== 'reserved') {
-        res.status(400).json({error: 'The seat cannot be paid for.'});
-        return;
-    }
+    router.post('/pay', (req, res) => {
+        const seatId = parseInt(req.body.seatId);
+        const userId = parseInt(req.body.userId);
+        const userEmail = req.body.email;
 
-    if (!userId) {
-        res.status(400).json({error: 'The seat cannot be reserved.'});
-        return;
-    }
+        connection.query('SELECT * FROM seats WHERE id = ? AND status = "reserved"', [seatId], (err, results) => {
+            if (err) {
+                console.error(err);
+                res.status(500).json({error: 'Failed to process payment.'});
+                return;
+            }
 
-    seat.status = 'sold';
-    seat["userId"] = userId;
+            if (results.length === 0) {
+                res.status(400).json({error: 'The seat cannot be paid for.'});
+                return;
+            }
 
-    // TODO: Implement email sending using Nodemailer
+            connection.query('UPDATE seats SET status = "sold", userId = ? WHERE id = ?', [userId, seatId], (err, updateResult) => {
+                if (err) {
+                    console.error(err);
+                    res.status(500).json({error: 'Failed to process payment.'});
+                    return;
+                }
 
-    res.json({message: 'Payment successful.'});
-});
+                // TODO: Implement email sending using Nodemailer
+
+                res.json({message: 'Payment successful.'});
+            });
+        });
+    });
+
+    router.post("/add_seat", async (req, res) => {
+        const query = "INSERT INTO seats (status) VALUES ('available')";
+
+        connection.query(query, (err, result) => {
+            if (err) {
+                console.error(err);
+                res.status(500).json({error: "Failed to add the seat."});
+                return;
+            }
+            const newSeatId = result.insertId;
+            res.json({id: newSeatId, status: "available"});
+        });
+    });
+    return router;
+};
 
 module.exports = seatRouter;

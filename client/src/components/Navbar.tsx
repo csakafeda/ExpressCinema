f8ab685cb99f54462a837466c5ff23b2b1a8ab77
csakafeda <bako.feodora97@gmail.com>
2023-07-09 16:32:32 +0200

    make footer and navbar more stylish

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,20 +1,22 @@
-    import {AppBar, Toolbar, Typography} from "@mui/material";
-    import React from "react";
+import {AppBar, Toolbar, Typography} from "@mui/material";
+import React from "react";
 
-    export const Navbar: React.FC = () => {
+export const Navbar: React.FC = () => {
 
-        return (
-            <AppBar position="static" color="primary">
-                <Toolbar style={{}}>
-                    <Typography variant="h6" component="div">
-                        <a href={"/"}>Express Cinema</a>
-                    </Typography>
-                    <Typography variant="h6" component="div">
-                        <a href="/src/pages/Reservation">
-                            Reserve
-                        </a>
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-        );
-    };
+    return (
+        <AppBar position="static" style={{backgroundColor: "#2B6BB2"}}>
+            <Toolbar style={{display: "flex", alignItems: "center", justifyContent: "space-around"}}>
+                <Typography variant="h6" component="div" style={{}}>
+                    <a href="/" style={{textDecoration: "none", color: "white"}}>
+                        Express Cinema
+                    </a>
+                </Typography>
+                <Typography variant="h6" component="div" style={{color: "white"}}>
+                    <a href="/reservation" style={{textDecoration: "none", color: "white"}}>
+                        Reserve
+                    </a>
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    );
+};

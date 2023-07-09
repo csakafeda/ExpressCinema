257279de0b7cbf522a19371a9b972849e58eff85
csakafeda <bako.feodora97@gmail.com>
2023-07-09 13:21:07 +0200

    add theme colorization, remove css-s

---
+++ client/src/Theme.js
@@ -0,0 +1,20 @@
+export const themeOptions = {
+    palette: {
+        primary: {
+            main: '#bfc6f5',
+        },
+        secondary: {
+            main: '#ec4c4c',
+        },
+        background: {
+            default: '#fde0e2',
+            paper: '#0003d2',
+        },
+        text: {
+            primary: '#000000',
+            secondary: '#ff1c6a',
+            disabled: '#7aff43',
+            hint: '#dfff00',
+        },
+    }
+};

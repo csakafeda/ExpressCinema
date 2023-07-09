357882dd0fd8d6bdc52f101eeb028a1543c1e91f
csakafeda <bako.feodora97@gmail.com>
2023-07-09 14:52:21 +0200

    make "/" the home route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
                 <Router>
                     <Navbar/>
                     <Routes>
-                        <Route path={"/home"} element={<Home/>}/>
+                        <Route path={"/"} element={<Home/>}/>
                         <Route path={"/reservation"} element={<Reservation/>}/>
                     </Routes>
                     <Footer/>

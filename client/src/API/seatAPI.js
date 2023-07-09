50349f20ab0d3556791b24046876a33e1f331195
csakafeda <bako.feodora97@gmail.com>
2023-07-09 17:53:54 +0200

    add reservation request

--- client/src/API/seatAPI.js
+++ client/src/API/seatAPI.js
@@ -9,3 +9,23 @@ export const getAllSeats = () => {
 export const purchaseSeats = (seats, formData) => {
 
 }
+
+export const reserveSeats = (seat, userId) => {
+    return fetch("http://localhost:8080/api/seats/reserve", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({seatId: seat, userId: userId})
+    })
+        .then((response) => {
+            if (response.ok) {
+                console.log("Reservation successful!");
+            } else {
+                console.error("Failed to make reservation.");
+            }
+        })
+        .catch((error) => {
+            console.error("Error making reservation:", error);
+        });
+}

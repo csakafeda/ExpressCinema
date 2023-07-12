f58c53b71ff5de58453cb6bb9768c9187ca9cfe2
csakafeda <bako.feodora97@gmail.com>
2023-07-12 11:28:29 +0200

    add basic socket.io connection to server

--- server/server.js
+++ server/server.js
@@ -1,18 +1,36 @@
 const express = require('express');
 const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const cors = require('cors');
 const seatRouter = require("./routes/seatRouter");
 const con = require("./config/db.js")
-const cors = require('cors');
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
+    req.io = io;
+    return next();
+});
+
+app.use(function (req, res, next) {
     req.con = con
     next()
-})
+});
 
 app.use("/api/seats", seatRouter);
 
-app.listen(8080, () => {
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log(`user connected ${socket.id}`)
+})
+
+server.listen(8080, () => {
     console.log('Server is running on port 8080.');
 });
